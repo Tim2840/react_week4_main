5d4f9f6d015ad4aa0e0d4e3ed4cf617664027e20
Guo Xuan-Ting <gg23456789tw@gmail.com>
2026-02-09 13:53:08 +0800

    Feat: Add pagination styles and enhance Pagination component structure

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -17,29 +17,24 @@ function Pagination({ pageInfo, handlePageChange }) {
             </a>
           </li>
 
-          {Array.from({ length: pageInfo.total_pages || 0 }).map(
-            (_, index) => (
-              console.log("原資料索引： ", index),
-              (
-                <li
-                  key={index}
-                  className={`page-item ${
-                    pageInfo.current_page === index + 1 && "active"
-                  }`}
-                >
-                  <a
-                    className="page-link"
-                    href="#"
-                    onClick={(e) => {
-                      handlePageChange(e, index + 1);
-                    }}
-                  >
-                    {index + 1}
-                  </a>
-                </li>
-              )
-            ),
-          )}
+          {Array.from({ length: pageInfo.total_pages || 0 }).map((_, index) => (
+            <li
+              key={index}
+              className={`page-item ${
+                pageInfo.current_page === index + 1 && "active"
+              }`}
+            >
+              <a
+                className="page-link"
+                href="#"
+                onClick={(e) => {
+                  handlePageChange(e, index + 1);
+                }}
+              >
+                {index + 1}
+              </a>
+            </li>
+          ))}
 
           <li className={`page-item ${!pageInfo.has_next && "disabled"}`}>
             <a
